18c6eb0f192c47240d448af6455ae954e1897ce0
ojedam13 <m_ojeda17@hotmail.com>
2022-01-21 22:44:28 -0300

    mostrar imagen likes views y link imagen

--- js/app.js
+++ js/app.js
@@ -40,15 +40,39 @@ function mostrarAlerta(mensaje) {
 
 function buscarImagenes(termino) {
     const key = '25354418-c88e4aeb2d3f9e0024f8911ac';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}
-`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(resultado => {
             mostrarImagenes(resultado.hits);
-        })
+        }) 
 }
 
 function mostrarImagenes(imagenes) {
-    
+    console.log(imagenes);
+
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+    //iterar sobre el arreglo de imagenes y construir el html
+    imagenes.forEach( imagen => {
+        const { previewURL, likes, views, largeImageURL } = imagen;
+        
+        resultado.innerHTML += `
+        <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+            <div class="bg-white">
+                <img class="w-full" src="${previewURL}">
+            
+                <div class="p-4">
+                    <p class="font-bold">${likes} <span class="font-light"> Me Gusta</span></p>
+                    <p class="font-bold">${views} <span class="font-light"> Veces Vista</span></p>
+                    <a class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
+                    href="${largeImageURL}" target="_blank" rel="noopener noreferrer">
+                    Ver Imagen
+                    </a>
+                </div>
+            </div>
+        </div>
+        `;
+    })
 }
