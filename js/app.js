63110f8c52825fd8fc55ee7933191aa94b3dd881
ojedam13 <m_ojeda17@hotmail.com>
2022-01-24 17:47:19 -0300

    navegando por las paginas

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const paginacionDiv = document.querySelector('#paginacion');
 const registrosPorPagina = 40;
 let totalPaginas;
 let iterador;
+let paginaActual = 1;
 
 window.onload = () => {
     formulario.addEventListener('submit', validarFormulario);
@@ -43,9 +44,11 @@ function mostrarAlerta(mensaje) {
     
 }
 
-function buscarImagenes(termino) {
+function buscarImagenes() {
+    const termino = document.querySelector('#termino').value;
+
     const key = '25354418-c88e4aeb2d3f9e0024f8911ac';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(resultado => {
@@ -116,7 +119,13 @@ function imprimirPaginador() {
         boton.href = '#';
         boton.dataset.pagina = value;
         boton.textContent = value;
-        boton.classList.add('siguiente', 'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-4', 'uppercase', 'rounded');
+        boton.classList.add('siguiente', 'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-4', 'rounded');
+
+        boton.onclick = () => {
+            paginaActual = value;
+
+            buscarImagenes();
+        }
 
         paginacionDiv.appendChild(boton);
     }
